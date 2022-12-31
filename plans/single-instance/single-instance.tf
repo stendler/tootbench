61bcb239a1332da508958944789adbc40538b7de
stendler <git@stendler.de>
2022-12-31 01:39:58 +0100

    Terraform: add controller instance and allow internal web & ping traffic

--- plans/single-instance/single-instance.tf
+++ plans/single-instance/single-instance.tf
@@ -25,7 +25,31 @@ data "template_file" "user_data" {
 resource "google_compute_instance" "instance" {
   machine_type = "e2-micro"
   name         = "mstdn-single-instance"
-  tags         = ["ssh"]
+  tags         = ["ssh", "internal"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2210-amd64"
+    }
+  }
+
+  metadata = {
+    user-data = data.template_file.user_data.rendered
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+      # Include this section to give the VM an external IP address
+      network_tier = "STANDARD"
+    }
+  }
+}
+
+resource "google_compute_instance" "controller" {
+  machine_type = "e2-micro"
+  name         = "controller"
+  tags         = ["ssh", "internal"]
 
   boot_disk {
     initialize_params {
@@ -57,6 +81,22 @@ resource "google_compute_firewall" "ssh" {
   priority      = 1000
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["ssh"]
+}
+
+resource "google_compute_firewall" "internal" {
+  name = "allow-net-internal"
+  allow {
+    ports    = ["80", "443"]
+    protocol = "tcp"
+  }
+  allow {
+    protocol = "icmp"
+  }
+  direction     = "INGRESS"
+  network       = google_compute_network.vpc_network.id
+  priority      = 1000
+  source_tags   = ["internal"]
+  target_tags   = ["internal"]
 }
 
 // A variable for extracting the external IP address of the VM
