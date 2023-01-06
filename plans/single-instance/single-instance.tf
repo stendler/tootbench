6537970498d1024e2500e1bac475bcc6b93695f6
stendler <git@stendler.de>
2023-01-06 22:54:43 +0100

    Terraform: make mastodon instance available to external traffic for manual testing

--- plans/single-instance/single-instance.tf
+++ plans/single-instance/single-instance.tf
@@ -106,7 +106,7 @@ data "cloudinit_config" "controller" {
 resource "google_compute_instance" "instance" {
   machine_type = "e2-medium"
   name         = var.instance-name
-  tags         = ["ssh", "internal"]
+  tags         = ["ssh", "internal", "debug-extern"]
   allow_stopping_for_update = true
 
   boot_disk {
@@ -181,6 +181,27 @@ resource "google_compute_firewall" "internal" {
   priority      = 1000
   source_tags   = ["internal"]
   target_tags   = ["internal"]
+}
+
+resource "google_compute_firewall" "extern" {
+  name = "allow-web-extern-debug"
+  allow {
+    ports    = ["80", "443"]
+    protocol = "tcp"
+  }
+  allow {
+    protocol = "icmp"
+  }
+  direction     = "INGRESS"
+  network       = google_compute_network.vpc_network.id
+  priority      = 1000
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["debug-extern"]
+}
+
+resource "local_file" "ip" {
+  filename = "ip"
+  content = google_compute_instance.instance.network_interface.0.access_config.0.nat_ip
 }
 
 // A variable for extracting the external IP address of the VM
--- scripts/await-ssh.sh
+++ scripts/await-ssh.sh
@@ -4,10 +4,11 @@ gcloud compute config-ssh --ssh-key-file=.ssh/id_ed25519
 
 # await ssh-server ready for connections
 # https://stackoverflow.com/questions/54668239/how-to-wait-until-ssh-is-available
-until gcloud compute ssh mstdn-single-instance --ssh-key-file=.ssh/id_ed25519 --command=true -- -o ConnectTimeout=5 2>&1; do
+until gcloud compute ssh ansible@mstdn-single-instance --ssh-key-file=.ssh/id_ed25519 --command="test -f .env.production" -- -o ConnectTimeout=5 2>&1; do
   sleep 1
   echo "Trying to connect..."
 done
 
+gcloud compute ssh ansible@mstdn-single-instance --ssh-key-file=.ssh/id_ed25519 --command="sed -i \"/^ALTERNATE_DOMAINS=/ s/$/$(cat plans/single-instance/ip)/\" .env.production"
 (gcloud compute ssh mstdn-single-instance --ssh-key-file=.ssh/id_ed25519 --command="tail -f /var/log/cloud-init-output.log" &) | awk '{print}; /cloud-init has finished/{exit}'
 gcloud compute ssh controller --ssh-key-file=.ssh/id_ed25519 --command="https mstdn-single-instance --ignore-stdin"
