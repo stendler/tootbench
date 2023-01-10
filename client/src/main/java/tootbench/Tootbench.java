fe8253b45dbc13aa70bd9d305cfd6b94cfda1658
stendler <git@stendler.de>
2023-01-10 20:26:19 +0100

    client: rename App and split logic from main/run instructions

--- client/src/main/java/tootbench/Tootbench.java
+++ client/src/main/java/tootbench/Tootbench.java
@@ -7,28 +7,21 @@ import com.sys1yagi.mastodon4j.api.Shutdownable;
 import com.sys1yagi.mastodon4j.api.entity.auth.AppRegistration;
 import com.sys1yagi.mastodon4j.api.exception.Mastodon4jRequestException;
 import com.sys1yagi.mastodon4j.api.method.Apps;
-import com.sys1yagi.mastodon4j.api.method.Follows;
-import com.sys1yagi.mastodon4j.api.method.Statuses;
 import com.sys1yagi.mastodon4j.api.method.Streaming;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.OkHttpClient;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 @Slf4j
-public class TootClientApp {
+public class Tootbench {
 
   public static final String CLIENT_NAME = "Tootbenchamun";
 
@@ -38,20 +31,6 @@ public class TootClientApp {
 
   private final List<User> users = new ArrayList<>();
 
-
-  public static void addCertificate() {
-    try (var certFile = TootClientApp.class.getClassLoader().getResourceAsStream("minica.der")) {
-      var cert = (X509Certificate) CertificateFactory.getInstance("X.509").generateCertificate(certFile);
-      var keystore = KeyStore.getInstance(KeyStore.getDefaultType());
-      keystore.load(new FileInputStream(System.getProperty("java.home") + "/lib/security/cacerts"), "changeit".toCharArray());
-      keystore.setCertificateEntry("minica", cert);
-      log.info("added cert");
-    } catch (IOException e) {
-      throw new UncheckedIOException(e);
-    } catch (CertificateException | KeyStoreException | NoSuchAlgorithmException e) {
-      throw new RuntimeException(e);
-    }
-  }
 
   public RegisteredApp register(String host, String clientName) {
     try {
@@ -107,74 +86,6 @@ public class TootClientApp {
       log.error("No app registered yet for host {}", host);
       throw new RuntimeException(e);
     }
-  }
-
-  public static void test() {
-    var host = "mstdn-single-instance";
-
-    var toot = new TootClientApp();
-    var app = toot.register(host, CLIENT_NAME);
-
-    List<Shutdownable> toBeGracefullyShutdowned = new ArrayList<>();
-
-    var username = "user1@localhost";
-    var user1 = toot.loginUser(host, username, "2e6bbb94173971027c4207af64e061c6");
-    toBeGracefullyShutdowned.add(user1.feedStream);
-
-    var user2 = toot.loginUser(host, "user2@localhost", "91c989c55a3d5e3163d6495c264c78c2");
-    new Follows(user2.clientSender()).postRemoteFollow("user1@localhost");
-    new Follows(user1.clientSender()).postRemoteFollow("user2@localhost");
-
-    try {
-      var userSender = new Statuses(user1.clientSender());
-      log.info("start posting");
-      int post = 400;
-      for (int i = 0; i < post; i++) {
-        var status = userSender.postStatus("my cool status " + i, null, null, false, null).execute();
-        log.info("Posted status\t{}\t{}", status.getAccount().getAcct(), status.getCreatedAt());
-      }
-
-      Thread.sleep(100000L);
-    } catch (Mastodon4jRequestException e) {
-      log.error("Request error. Probably rate limit exceeded. Shutting down...");
-      toBeGracefullyShutdowned.forEach(Shutdownable::shutdown);
-      log.debug(e.getResponse().toString());
-    } catch (InterruptedException e) {
-      log.error("Sending interrupted.");
-    }
-
-    try {
-      var userSender = new Statuses(user2.clientSender());
-      var status = userSender.postStatus("my cool status xxx", null, null, false, null).execute();
-      log.info("Posted status\t{}\t{}", status.getAccount().getAcct(), status.getCreatedAt());
-    } catch (Mastodon4jRequestException e) {
-      log.error("user 2 cannot post either...");
-    }
-  }
-
-  /**
-   * todo Log users in and safe tokens and client id & secret to file
-   */
-  public static void login() {
-    throw new UnsupportedOperationException("Not yet implemented");
-  }
-
-  public static void run() {
-
-  }
-
-  public static void main(String[] args) {
-
-    addCertificate();
-
-    var argBuffer = new StringBuilder();
-    Arrays.stream(args).forEach(arg -> argBuffer.append(arg).append(" "));
-    switch (argBuffer.toString()) {
-      case String s when s.contains("--run") -> run();
-      case String s when s.contains("--login") -> login();
-      case default -> test();
-    }
-
   }
 
   public record User(Shutdownable feedStream, MastodonClient clientSender) {}
