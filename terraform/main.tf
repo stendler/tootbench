05d5421575d82547af67e13e77af5c270a64e981
stendler <git@stendler.de>
2023-01-19 15:49:22 +0100

    terraform: move scenario config into its own file

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,57 +12,6 @@ variable "ansible-ssh-key-file" {
   default = "../.ssh/id_ed25519.pub"
 }
 
-variable "scenario" {
-  type = object({
-    name = string
-    client_machine_type = string
-    instance_machine_type = string
-    instances = list(object({
-      users = number # todo obsolete by using max(posting_users, listening_users) instead
-      posting_users = number # number of users that post
-      listening_users = number # number of users that listen
-    }))
-  })
-
-  validation {
-    condition = length([ for i in var.scenario.instances: i
-      if i.users % 1 != 0 || i.listening_users % 1 != 0 || i.posting_users % 1 != 0
-    ]) == 0
-    error_message = "All user numbers must be integers."
-  }
-
-  validation {
-    condition = length([ for i in var.scenario.instances: i
-      if i.users < i.posting_users || i.users < i.listening_users
-    ]) == 0
-    error_message = "Number of listening users and number of posting users per instance must be less or equal the number of users of that instance."
-  }
-
-  description = <<-EOT
-  Benchmark scenario configuration.
-
-  name: of the scenario and used as instance names and for the collected output file directory.
-  client_machine_type: gcp machine type for the client machine. Preferably many core less ram.
-  instance_machine_type: gcp machine type for the instances. Preferably multi-core and no burst machines.
-  instances: list of instances with per instance configuration
-    posting_users: number of users on this instance, creating posts
-    listening_users: number of users on this instance, listening to a feed stream
-  EOT
-
-  default = {
-    name = "single-instance-default"
-    client_machine_type = "e2-micro"
-    instance_machine_type = "e2-standard-2"
-    instances = [
-      {
-        users = 10
-        posting_users = 10
-        listening_users = 10
-      }
-    ]
-  }
-}
-
 resource "google_compute_network" "vpc_network" {
   name                    = "mstdn-single-network"
   auto_create_subnetworks = "true"
--- terraform/scenario-config.tf
+++ terraform/scenario-config.tf
@@ -1,23 +1,52 @@
 
-variable "scenario-config" {
+variable "scenario" {
   type = object({
-    instance_machine_type = string
+    name = string
     client_machine_type = string
+    instance_machine_type = string
     instances = list(object({
-      name = string
-      number_of_users = number
+      users = number # todo obsolete by using max(posting_users, listening_users) instead
+      posting_users = number # number of users that post
+      listening_users = number # number of users that listen
     }))
     # todo maybe clients (?)
   })
 
+  validation {
+    condition = length([ for i in var.scenario.instances: i
+      if i.users % 1 != 0 || i.listening_users % 1 != 0 || i.posting_users % 1 != 0
+    ]) == 0
+    error_message = "All user numbers must be integers."
+  }
+
+  validation {
+    condition = length([ for i in var.scenario.instances: i
+      if i.users < i.posting_users || i.users < i.listening_users
+    ]) == 0
+    error_message = "Number of listening users and number of posting users per instance must be less or equal the number of users of that instance."
+  }
+
+  description = <<-EOT
+  Benchmark scenario configuration.
+
+  name: of the scenario and used as instance names and for the collected output file directory.
+  client_machine_type: gcp machine type for the client machine. Preferably many core less ram.
+  instance_machine_type: gcp machine type for the instances. Preferably multi-core and no burst machines.
+  instances: list of instances with per instance configuration
+    posting_users: number of users on this instance, creating posts
+    listening_users: number of users on this instance, listening to a feed stream
+  EOT
+
   default = {
+    name = "single-instance-default"
+    client_machine_type = "e2-micro"
     instance_machine_type = "e2-standard-2"
-    client_machine_type = "e2-custom-4-2048"
     instances = [
       {
-        name : "instance"
-        number_of_users : 10
-      },
-      ]
+        users = 10
+        posting_users = 10
+        listening_users = 10
+      }
+    ]
   }
 }
