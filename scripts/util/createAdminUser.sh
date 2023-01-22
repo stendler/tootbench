28d0b869c702191f9730f73b1fa820b2741a635e
stendler <git@stendler.de>
2023-01-22 18:29:11 +0100

    mastodon: switch version from v4.0.2 to v3.5.5 - test if federation is easier to trigger?

--- scripts/util/createAdminUser.sh
+++ scripts/util/createAdminUser.sh
@@ -11,7 +11,7 @@ if [ -z "$domain" ]; then
 fi
 
 echo "➔ creating owner $username with email $username@$domain"
-newuser=$(docker compose --project-name mastodon run -it --rm --name "$domain" tootctl accounts create $username --email $username@$domain --confirmed --role Owner | awk -v user=$username -v email="$username@$domain" '/New password:/ {print user, email, $3}')
+newuser=$(docker compose --project-name mastodon run -it --rm --name "$domain" tootctl accounts create $username --email $username@$domain --confirmed --role admin | awk -v user=$username -v email="$username@$domain" '/New password:/ {print user, email, $3}')
 
 if [ -z "$newuser" ]; then
   echo "User creation failed. Already exists?"
