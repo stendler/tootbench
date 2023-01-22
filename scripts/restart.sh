9ac881d0832e5f9e71798c38deb0d9ac8d793b58
stendler <git@stendler.de>
2023-01-22 23:02:08 +0100

    fix restart script for multiple instances

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -3,12 +3,15 @@
 set -e
 
 # only checking one arg is enough, since there are only 2 total and both is the default
-instances="$1"
-if [ -z $instances ]; then
-  instances="instance client"
+resources="$1"
+if [ -z $resources ]; then
+  resources="instance client"
 fi
 
+let n=$(cat terraform/hosts | wc -l)-1
+resources=$(echo $resources | sed --expression "s/instance/$(for i in $(seq 0 $n); do printf instance[$i]\ ; done)/")
+
 terraform -chdir=terraform init
-terraform -chdir=terraform apply $scenario_cmd $(for instance in $instances; do echo -replace=google_compute_instance.$instance; done) -auto-approve
+terraform -chdir=terraform apply $scenario_cmd $(for resource in $resources; do echo -replace="google_compute_instance.$resource"; done) -auto-approve
 
 ./scripts/await-ssh.sh
