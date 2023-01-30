a013f3e9d9ea48306fe724d4630ee44e65e9da2e
stendler <git@stendler.de>
2023-01-30 23:27:52 +0100

    analysis preprocessing: filter docker-stats output

--- analysis/preprocess.sh
+++ analysis/preprocess.sh
@@ -6,3 +6,4 @@ find analysis/input/*/ -type f -name vmstat.log.gz -exec sh -c 'outputfile=$(ech
 find analysis/input/*/ -type f -name iostat-cpu.log.gz -exec sh -c 'outputfile=$(echo "{}" | sed "s/input/processed/"); mkdir -p $(dirname $outputfile); ./analysis/preprocessing/iostat-cpu.sh "{}" > $outputfile'  \;
 find analysis/input/*/ -type f -name iostat-disk.log.gz -exec sh -c 'outputfile=$(echo "{}" | sed "s/input/processed/"); mkdir -p $(dirname $outputfile); ./analysis/preprocessing/iostat-disk.sh "{}" > $outputfile'  \;
 find analysis/input/*/ -type f -name mpstat.log.gz -exec sh -c 'outputfile=$(echo "{}" | sed "s/input/processed/"); mkdir -p $(dirname $outputfile); ./analysis/preprocessing/mpstat.sh "{}" > $outputfile'  \;
+find analysis/input/*/ -type f -name docker-stats.log.gz -exec sh -c 'outputfile=$(echo "{}" | sed "s/input/processed/"); mkdir -p $(dirname $outputfile); ./analysis/preprocessing/docker-stats.sh "{}" > $outputfile'  \;
--- analysis/preprocessing/docker-stats.sh
+++ analysis/preprocessing/docker-stats.sh
@@ -5,7 +5,7 @@
 if [ -f "$1" ]; then
   # header
   echo "timestamp,host,process,container_id,name,cpu_pct,mem_usage,mem_limit,mem_pct,net_input,net_output,block_input,block_output,pid" | gzip
-  zcat "$1" | grep -v "blob data" | sed -E 's/\//;s/\s+/,/g' | tail -n +2 | gzip
+  zcat "$1" | grep -v "blob data" | grep -v "systemd" | grep -v "CONTAINER" | grep -v " -- " | sed -E 's/\///g;s/\s+/,/g' | tail -n +2 | gzip
 else
   echo 1>&2 "docker stats log file not found"
   exit 1
