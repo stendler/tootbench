acd7fd7dbf39682e9fdc900905e045fead8d66bd
stendler <git@stendler.de>
2023-02-03 01:09:34 +0100

    scripts: parallelize local follow action
    does not seem to have much of an impact

--- scripts/util/followersAddLocal.sh
+++ scripts/util/followersAddLocal.sh
@@ -8,5 +8,7 @@ fi
 # will that even work for remote users?
 # local can only be added with $username and not via $username@domain
 for username in $(cat "$1" | awk '{print $1}'); do
-  docker compose --project-name mastodon run --rm tootctl accounts follow $username
+  docker compose --project-name mastodon run --rm tootctl accounts follow $username &
 done
+
+wait
