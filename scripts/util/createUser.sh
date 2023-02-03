d7b221ac0de694588f8376cb27ba8c58ba41b165
stendler <git@stendler.de>
2023-02-03 01:08:18 +0100

    Ansible: reduce create user time by not creating a new container for each user
    at least for about 10 seconds

--- scripts/util/createUser.sh
+++ scripts/util/createUser.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+#
+# Create users. Needs tootctl - e.g. via docker compose --project-name mastodon run --rm --name {{ hostname }} -v $(pwd)/createUser.sh:/createUser.sh --entrypoint=/createUser.sh tootctl {{ users }} user {{ hostname }} > users.txt
+#
 
 number="$1"
 if [ -z "$number" ]; then
@@ -15,16 +18,16 @@ if [ -z "$domain" ]; then
   domain="localhost"
 fi
 
-for i in `seq $number`; do
+for i in $(seq $number); do
   username=$user$i
-  echo "➔ creating user $username with email $username@$domain"
-  newuser=$(docker compose --project-name mastodon run --rm --name "$domain" tootctl accounts create $username --email $username@$domain --confirmed | awk -v user=$username -v email="$username@$domain" '/New password:/ {print user, email, $3}')
+  echo 1>&2 "➔ creating user $username with email $username@$domain"
+  newuser=$(tootctl accounts create $username --email $username@$domain --confirmed | awk -v user=$username -v email="$username@$domain" '/New password:/ {print user, email, $3}')
 
   if [ -z "$newuser" ]; then
-    echo "User creation failed. Already exists?"
+    echo 1>&2 "User creation failed. Already exists?"
     exit 1
   fi
 
-  echo $newuser >> users.txt
-  echo Created user: $newuser
+  echo "$newuser"
+  echo 1>&2 "Created user: $newuser"
 done
