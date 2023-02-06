8ce054b685787b97462921bd371b6ed020564194
stendler <git@stendler.de>
2023-02-07 00:05:34 +0100

    [Ansible] add ping service for network latency monitoring

---
+++ analysis/preprocessing/ping.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# Cleanup and convert the merged ping@ journalctl output to csv
+
+if [ -f "$1" ]; then
+  # header
+  echo "scenario,run,timestamp,timestamp_micro,host,process,size_bytes,target,ip,icmp_seq,ttl,time,time_unit" | gzip
+  zcat "$1" | grep -v "systemd" | grep -v "PING" | sed -E 's/bytes from//;s/\(|\)//g;s/icmp_seq=//;s/ttl=//;s/time=//;s/\s+/,/g' | gzip
+else
+  echo 1>&2 "ping log file not found"
+  exit 1
+fi
