aa4ac99958f3f461f51072f3d6111f7d96f91e86
stendler <git@stendler.de>
2023-02-06 17:52:37 +0100

    [analysis] add filters for the DataFrame to be passed to the plotting function

---
+++ analysis/lib/filter.py
@@ -0,0 +1,56 @@
+from typing import Callable
+
+from pandas import DataFrame
+
+
+def none(df: DataFrame) -> DataFrame:
+    return df
+
+
+def of(*args: (Callable[[DataFrame], DataFrame])) -> Callable[[DataFrame], DataFrame]:
+    def iter_filter(df: DataFrame) -> DataFrame:
+        ret = df
+        for filter in args:
+            ret = filter(ret)
+        return ret
+    return iter_filter
+
+
+def either(column: str, *args) -> Callable[[DataFrame], DataFrame]:
+    def condition_iter(df: DataFrame) -> DataFrame:
+        cond = None
+        for value in args:
+            if cond is None:
+                cond = df[column] == value
+            else:
+                cond |= df[column] == value
+        return df[cond]
+    return condition_iter
+
+
+def client(df: DataFrame) -> DataFrame:
+    return df[df["host"] == "client"]
+
+
+def instances(df: DataFrame) -> DataFrame:
+    return df[df["host"] != "client"]
+
+
+def scenario(name: str) -> Callable[[DataFrame], DataFrame]:
+    return column("scenario", name)
+
+
+def scenarios(*args: (str)) -> Callable[[DataFrame], DataFrame]:
+    return either("scenario", *args)
+
+
+def run(name: str = "1") -> Callable[[DataFrame], DataFrame]:
+    return column("run", name)
+
+
+def column(column: str, value: str) -> Callable[[DataFrame], DataFrame]:
+    def column_filter(df: DataFrame) -> DataFrame:
+        return df[df[column] == value]
+    return column_filter
+
+
