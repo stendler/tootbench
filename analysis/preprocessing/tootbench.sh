a3fa242b78c90452f5c1d94b51583b8f649b3697
stendler <git@stendler.de>
2023-02-08 04:42:37 +0100

    [preprocessing] split username and domain in client log csv

--- analysis/preprocessing/tootbench.sh
+++ analysis/preprocessing/tootbench.sh
@@ -4,8 +4,8 @@
 
 if [ -f "$1" ]; then
   # header
-  echo "scenario,run,timestamp_iso,message_type,message_len,sender_username,message_timestamp,server_timestamp,receiver_username" | gzip
-  cat "$1"
+  echo "scenario,run,timestamp_iso,message_type,message_len,sender_username,sender_domain,message_timestamp,server_timestamp,receiver_username,receiver_domain" | gzip
+  zcat "$1" | sed -E "s/@/,/g" | gzip
 else
   echo 1>&2 "client log file not found"
   exit 1
