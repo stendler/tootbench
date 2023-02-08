f1e6d093f478da5b1df4f4204805f0e44bc534e2
stendler <git@stendler.de>
2023-02-08 19:31:42 +0100

    [analysis] window: always default to minutes in result

--- analysis/lib/window.py
+++ analysis/lib/window.py
@@ -13,9 +13,7 @@ def max(df_group: pd.core.groupby.GroupBy) -> DataFrame:
     return df_group.max()
 
 
-def tumble(columns: [str] = ["scenario", "run", "host"], time_column: str = "time", unit: str = "s", freq: str = "5s", new_unit: str = None, func: Callable[[pd.core.groupby.GroupBy], DataFrame] = mean) -> Callable[[DataFrame], DataFrame]:
-    if new_unit is None:
-        new_unit = unit
+def tumble(columns: [str] = ["scenario", "run", "host"], time_column: str = "time", unit: str = "s", freq: str = "5s", new_unit: str = "m", func: Callable[[pd.core.groupby.GroupBy], DataFrame] = mean) -> Callable[[DataFrame], DataFrame]:
     def tumbling_window(df: DataFrame) -> DataFrame:
         ret_df = df.copy()
         # first turn the time_column in to a datetime64
