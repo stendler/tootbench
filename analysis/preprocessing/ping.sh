6e21a3a3a51d4d2a26da63c92245b8bc5ab42986
stendler <git@stendler.de>
2023-02-08 04:42:12 +0100

    [preprocessing] fix ping column naming and add forgotten timestamp split

--- analysis/preprocessing/ping.sh
+++ analysis/preprocessing/ping.sh
@@ -4,8 +4,8 @@
 
 if [ -f "$1" ]; then
   # header
-  echo "scenario,run,timestamp,timestamp_micro,host,process,size_bytes,target,ip,icmp_seq,ttl,time,time_unit" | gzip
-  zcat "$1" | grep -v "systemd" | grep -v "PING" | sed -E 's/bytes from//;s/\(|\)//g;s/icmp_seq=//;s/ttl=//;s/time=//;s/\s+/,/g' | gzip
+  echo "scenario,run,timestamp,timestamp_micro,host,process,size_bytes,target,ip,icmp_seq,ttl,ping_time,ping_time_unit" | gzip
+  zcat "$1" | grep -v "systemd" | grep -v "PING" | sed -E 's/\./,/;s/bytes from//;s/\(|\)|://g;s/icmp_seq=//;s/ttl=//;s/time=//;s/\s+/,/g' | gzip
 else
   echo 1>&2 "ping log file not found"
   exit 1
